70cb9caa96720143b0288c6a34a4de0f0d1dfde4
Johannes Maibaum <jmaibaum@gmail.com>
2019-10-30 15:14:41 +0100

    Don't call bindgen in build.rs
    Add generated bindings.rs to tree instead.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bindings to libltc
+
+## Bindgen commandline
+
+The following commandline was used to create the initial bindings:
+
+```bash
+bindgen vendor/src/ltc.h -o src/bindings.rs
+```
--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
-use std::env;
-use std::path::PathBuf;
-
 fn main() {
     // Build libltc
     let src = [
@@ -13,16 +10,4 @@ fn main() {
     let mut builder = cc::Build::new();
     let build = builder.files(src.iter()).include("vendor/src");
     build.compile("ltc");
-
-    let bindings = bindgen::Builder::default()
-        .header("vendor/src/ltc.h")
-        // FIXME: Wait for next bindgen release, see: https://github.com/rust-lang/rust-bindgen/issues/1647
-        //.parse_callbacks(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
